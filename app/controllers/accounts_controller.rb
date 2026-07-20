0e91dd222092967426cce3e067dbc46bcf9582c1
Ben Ehmke <benehmke@gmail.com>
2026-07-20 10:19:23 -0500

    add accounts edit

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,9 @@
 class AccountsController < ApplicationController
+
+  # StandardAPI has no `edit` action, so define one that loads @account for the
+  # edit template (mirrors its `show`).
+  def edit
+    @account = resource
+  end
+
 end
